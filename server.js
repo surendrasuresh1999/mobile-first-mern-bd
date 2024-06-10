f34472b69ce5481ed27951abf030fa3b38146bf1
surendrasuresh1999 <k.surendraparla1999@gmail.com>
2024-06-10 12:08:08 +0530

    feat://project intialized

---
+++ server.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const blogRoute = require("./Routes/blogRoutes");
+const quoteRoute = require("./Routes/quoteRoutes");
+const commentRoute = require("./Routes/commentRoutes");
+const userRoutes = require("./Routes/userRoutes");
+
+// express app
+const app = express();
+app.use(cors());
+
+// middleware function
+app.use(express.json());
+
+app.use((req, res, next) => {
+  next();
+});
+
+// routes
+app.use("/api/blog", blogRoute);
+app.use("/api/user", userRoutes);
+
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("mongoose connected successfully");
+    // listen for requests
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
